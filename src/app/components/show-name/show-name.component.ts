1394886f5934c9f4a5e0b80e305cc01f5d8ab28a
Roger-Rys <roger.reyesm97@gmail.com>
2022-04-27 22:29:23 -0500

    OnChanges

--- src/app/components/show-name/show-name.component.ts
+++ src/app/components/show-name/show-name.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-show-name',
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class ShowNameComponent implements OnInit {
+export class ShowNameComponent implements OnInit, OnChanges {
+
+  @Input() name!: string;
 
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
 
   ngOnInit(): void {
   }
